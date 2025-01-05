5443a266a8c7e57f44734624fb5e4070e53d8630
ShivaramV3223 <shivaramvelayutham3223@gmail.com>
2025-01-05 18:05:54 +0530

    adding files

---
+++ app/js/script.js
@@ -0,0 +1,26 @@
+const btnhamburger = document.querySelector('#btnhamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const menu = document.querySelector('.header__menu');
+const body = document.querySelector('body');
+
+btnhamburger.addEventListener('click', function(){
+    if( header.classList.contains('open')){
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+        
+        menu.classList.remove('fade-in');
+        menu.classList.add('fade-out');
+    }
+    else{
+        body.classList.add('noscroll');
+        header.classList.add("open");
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+        
+        menu.classList.remove('fade-out');
+        menu.classList.add('fade-in');
+    }
+})
